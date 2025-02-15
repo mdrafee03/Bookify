a872f0f3e7eba406de55300ce0599b12abf21e1d
Md Rafee <md.rafee@7peakssoftware.com>
2025-02-16 02:47:17 +0700

    feat: add migration for roles

---
+++ src/Bookify.Infrastructure/Migrations/20250215165852_PopulateAdminRoleToRoles.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookify.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulateAdminRoleToRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_role_user_role_user_roles_id",
+                table: "role_user");
+
+            migrationBuilder.InsertData(
+                table: "roles",
+                columns: new[] { "id", "name" },
+                values: new object[] { 2, "Admin" });
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_role_user_roles_user_roles_id",
+                table: "role_user",
+                column: "user_roles_id",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_role_user_roles_user_roles_id",
+                table: "role_user");
+
+            migrationBuilder.DeleteData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: 2);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_role_user_role_user_roles_id",
+                table: "role_user",
+                column: "user_roles_id",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
